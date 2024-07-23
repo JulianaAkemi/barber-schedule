81f05e4930d3651a7847f35a3a3b36cb195f989e
Juliana <julianaakkemi@gmail.com>
2024-07-23 19:17:16 -0300

    feat: add basic types on core

---
+++ packages/core/src/employee/Employee.ts
@@ -0,0 +1,8 @@
+export default interface Employee {
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+  score: number;
+  qtyScore: number;
+}
---
+++ packages/core/src/task/Task.ts
@@ -0,0 +1,8 @@
+export default interface Task {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  slots: number;
+  imageUrl: string;
+}
