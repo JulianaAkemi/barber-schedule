5327888da3e072b8d8d0c0eea694e27630312e82
Juliana <julianaakkemi@gmail.com>
2024-07-24 19:28:33 -0300

    feat: add landing page overall layout

--- apps/frontend/src/app/(pages)/(landing)/page.tsx
+++ apps/frontend/src/app/(pages)/(landing)/page.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import TitleSlogan from "@/components/landing/TilteSlogan";
+import ContainerWithBackground from "@/components/shared/ContainerWithBackground";
+
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      <TitleSlogan />
+
+      <ContainerWithBackground image="/banners/servicos.webp">
+        Services we offer
+      </ContainerWithBackground>
+
+      <ContainerWithBackground image="/banners/profissionais.webp">
+        Our History
+      </ContainerWithBackground>
+
+      <ContainerWithBackground image="/banners/clientes.webp">
+        Our Clients
+      </ContainerWithBackground>
+    </div>
+  );
 }
